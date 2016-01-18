38c22a82ee3ee31c0e67dabf9425858001981e9a
harryggg <ma.tanghao@dhs.sg>
2016-01-18 18:11:46 +0800

    register page skeleton

--- controller/RegisterController.js
+++ controller/RegisterController.js
@@ -1,7 +1,14 @@
 app.controller("registerCtrl",function($scope)
 {
-  $scope.registerUser = function(userDetails) {
-    $scope.message = userDetails.name + userDetails.email+userDetails.agreed;
+  $scope.registerUser = function(newUser) {
+    $scope.message = newUser.name + newUser.email+newUser.agreed;
+    console.log(newUser.nationResidence);
   }
+  $scope.data={
+    nations: [
+      {id: '1', name: 'China'},
+      {id: '2', name: 'Singapore'},
+      {id: '3', name: 'Taiwan'}
+    ]};
   $scope.message = "Ready";
 });
