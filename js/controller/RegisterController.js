42633c51651a10c96dcc813b3d05265caa7b675a
harryggg <ma.tanghao@dhs.sg>
2016-01-22 14:24:31 +0800

    password confirmation in register form

--- js/controller/RegisterController.js
+++ js/controller/RegisterController.js
@@ -45,3 +45,24 @@ app.controller("RegisterController", ['$scope',
   $scope.message = "Ready";
 
 }]);
+
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive("compareTo", compareTo);
