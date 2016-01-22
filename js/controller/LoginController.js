1a9499299975a950e6522f676b06a8c39f0e3e47
Koh Jing Yu <kohjingyu95@gmail.com>
2016-01-22 22:48:57 +0800

    Updated login page

--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -2,7 +2,30 @@ app.controller("LoginController", ['$scope',
   function($scope)
 {
   $scope.loginUser = function(userDetails) {
-    $scope.message = userDetails.name + userDetails.email + userDetails.agreed;
+    $scope.message = userDetails.username + userDetails.password;
   }
+
   $scope.message = "Ready";
+  $scope.quote = {text: "Blah blah blah", author: "Ma Tanghao"};
 }]);
+
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive("compareTo", compareTo);
