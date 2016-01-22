08f87457c74a7980ed1bcc9193175f2d2fd11bdd
harryggg <ma.tanghao@dhs.sg>
2016-01-22 14:44:29 +0800

    login functional

--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -1,8 +1,8 @@
 app.controller("LoginController", ['$scope',
   function($scope)
 {
-  $scope.loginUser = function(userDetails) {
-    $scope.message = userDetails.name + userDetails.email + userDetails.agreed;
+  $scope.loginUser = function(user) {
+    console.log(user);
   }
   $scope.message = "Ready";
 }]);
