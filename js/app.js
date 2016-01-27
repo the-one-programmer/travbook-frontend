5e768bebd82c0589aaec16a12e85662027fd7f72
harryggg <ma.tanghao@dhs.sg>
2016-01-27 23:34:04 +0800

    login raw code

--- js/app.js
+++ js/app.js
@@ -24,3 +24,56 @@ app.config(function ($routeProvider)
     redirectTo: '/'
   });
 });
+
+app.factory("AuthService", function($http, $q, $rootScope, AuthToken, AuthEvents) {
+  return {
+    login: function(email, password) {
+      var d = $q.defer();
+      $http.post(BACKEND_URL+'login', {
+        email: email,
+        password: password
+      }).success(function(resp) {
+        console.log("success");
+        AuthToken.set(resp.auth_token);
+        $rootScope.$broadcast(AuthEvents.loginSuccess);
+        d.resolve(resp.user);
+      }).error(function(resp) {
+        $rootScope.$broadcast(AuthEvents.loginFailed);
+        d.reject(resp.error);
+      });
+      return d.promise;
+    }
+  };
+});
+
+app.factory("AuthInterceptor", function($q, $injector) {
+  return {
+    // This will be called on every outgoing http request
+    request: function(config) {
+      var AuthToken = $injector.get("AuthToken");
+      var token = AuthToken.get();
+      config.headers = config.headers || {};
+      if (token) {
+        config.headers.Authorization = "Bearer " + token;
+      }
+      return config || $q.when(config);
+    },
+    // This will be called on every incoming response that has en error status code
+    responseError: function(response) {
+      var AuthEvents = $injector.get('AuthEvents');
+      var matchesAuthenticatePath = response.config && response.config.url.match(new RegExp('/api/auth'));
+      if (!matchesAuthenticatePath) {
+        $injector.get('$rootScope').$broadcast({
+          401: AuthEvents.notAuthenticated,
+          403: AuthEvents.notAuthorized,
+          419: AuthEvents.sessionTimeout
+        }[response.status], response);
+      }
+      return $q.reject(response);
+    }
+  };
+});
+
+app.config(function($httpProvider) {
+  return $httpProvider.interceptors.push("AuthInterceptor");
+});
