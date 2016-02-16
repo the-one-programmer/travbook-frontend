7142badfdfb254d5141eed96a875b70c68d28480
Koh Jing Yu <kohjingyu95@gmail.com>
2016-02-16 20:13:59 +0800

    Added search function and updated nav bar link

--- js/controller/SearchController.js
+++ js/controller/SearchController.js
@@ -11,7 +11,7 @@ app.controller("SearchController",
 
   var currentUserURL = BACKEND_URL + 'current_user';
 
-$http({
+  $http({
     method: 'GET',
     url: currentUserURL,
     headers: {
@@ -24,98 +24,28 @@ $http({
     $scope.data.current_user_id = data.id;
 
     console.log(data.id);
+
+    // TODO: Load search results for query
+    $scope.searchQuery = $routeParams.query;
+
     // Get data of the profile currently being viewed
-    profileURL = BACKEND_URL + 'show/' + $scope.data.current_user_id;
+    searchURL = BACKEND_URL + 'search/';
     $http({
-      method: 'GET',
-      url: profileURL,
+      method: 'POST',
+      url: searchURL,
       headers: {
         'Authorization': $cookies.get("Travbook_auth_token")
       },
+      data: { "query": $routeParams.query }
     }).then(function successCallback(response) {
-      $scope.user = response.data;
-
-      // TODO: Load search results for query
-      $scope.searchQuery = $routeParams.query;
-      $scope.searchResults = [$scope.user];
-
-      $rootScope.username = $scope.user.name;
-
-      $scope.countrySelection = response.data.countries_want_to_go;
-      $scope.hobbySelection = response.data.interests;
-      $scope.languageSelection = response.data.languages;
-      console.log(response.data.countries_want_to_go);
-      
-      var nationURL = BACKEND_URL + "countries";
-      $http.get(nationURL).success(function (data){
-        $scope.data.nations = data;
-      //    $scope.data.cities = $scope.data.nations[$scope.user.nationResidence - 1].cities;
-
-          var countryData = getCountryForCityName($scope.user.city);
-          $scope.countryResidence = countryData[0].id;
-          $scope.cityResidence = countryData[1];
-          $scope.changeCitiesOption($scope.countryResidence);
-      }).error(function(error){
-        $scope.data.error = error;
-      });
-
-      var hobbyURL = BACKEND_URL + "interests";
-      $http.get(hobbyURL).success(function (data){
-        $scope.data.hobbies = data;
-      }).error(function(error){
-        $scope.data.error = error;
-      });
-
-      var languagesURL = BACKEND_URL + "languages";
-      $http.get(languagesURL).success(function (data){
-        $scope.data.languages = data;
-      }).error(function(error){
-        $scope.data.error = error;
-      });
+      console.log(response);
+      $scope.searchResults = response.users;
     }, function errorCallback(response) {
       console.log(response);
       // Error
       $scope.alertClass = "alert-danger";
       $scope.alertMessage = "There was an error. Please try again.";
-
-      // Redirect to profile after delay
-      $timeout(function() {
-        $scope.changeView('/');
-      }, 3000);
     });
-
-    var getCountryForCityName = function(cityName)
-    {
-      for(var i = 0; i < $scope.data.nations.length; i++)
-      {
-        var country = $scope.data.nations[i];
-        var cities = country.cities;
-
-        for(var j = 0; j < cities.length; j ++)
-        {
-          var current_city = cities[j];
-          if(current_city.name == cityName)
-          {
-            return [country, current_city];
-          }
-        }
-      }
-    }
-
-    var findCitiesByNationID = function(nationID){
-      console.log(nationID);
-      for( var i=0, l=$scope.data.nations.length; i<l; i++ ) {
-        if($scope.data.nations[i].id==nationID){
-          $scope.data.cities = $scope.data.nations[i].cities;
-          break;
-        }
-      }
-    }
-
-    $scope.changeCitiesOption = function(nationID)
-    {
-      findCitiesByNationID(nationID);
-    }
   })
   .error(function(response) {
     // Current user not logged in, redirect to login
@@ -138,21 +68,6 @@ $http({
   $scope.changeView = function(url)
   {
     $location.path(url);
-  }
-
-  $scope.wantsToGo = function(nation_id)
-  {
-    return $scope.user.countries_want_to_go.indexOf(nation_id) > -1
-  }
-
-  $scope.hasHobby = function(hobby_id)
-  {
-    return $scope.user.interests.indexOf(hobby_id) > -1
-  }
-
-  $scope.hasLanguage = function(language_id)
-  {
-    return $scope.user.languages.indexOf(language_id) > -1
   }
 });
 
